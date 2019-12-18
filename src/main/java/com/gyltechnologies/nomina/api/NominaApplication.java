3cada4b297de1505fdf9817553a3204023b7473d
luis.galeano <luchoingeniero@gmail.coms>
2019-12-17 20:59:43 -0500

    initial project

---
+++ src/main/java/com/gyltechnologies/nomina/api/NominaApplication.java
@@ -0,0 +1,13 @@
+package com.gyltechnologies.nomina.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NominaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NominaApplication.class, args);
+	}
+
+}
